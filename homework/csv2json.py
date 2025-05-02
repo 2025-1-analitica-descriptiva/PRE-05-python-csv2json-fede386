f9abe3862814b6699b56484249e85e6ad28c6630
fede386 <49293629+fede386@users.noreply.github.com>
2025-05-02 16:34:36 -0500

    solucion Final

--- homework/csv2json.py
+++ homework/csv2json.py
@@ -1,4 +1,5 @@
 """Script para convertir un archivo CSV a JSON"""
+
 import csv
 import json
 
@@ -8,6 +9,7 @@ def convert_csv_2_json(input_file):
     """Converts a CSV file to a JSON file"""
 
     output_file = input_file.replace(".csv", ".json")
+
     data = []
 
     with open(input_file, "r", encoding="utf-8") as f:
@@ -17,20 +19,30 @@ def convert_csv_2_json(input_file):
 
     with open(output_file, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4)
-        
+
     ui.notify("The file was transformed successfully!")
 
 
 def app():
+    
     ui.label("CSV to JSON Converter").classes("text-4xl font-bold")
+
+    ui.label("")
+
     filename = ui.input(
         label="CSV file to convert:",
         placeholder="filename",
     )
+    
+    ui.label("")
+
+    ui.label("")
+
     ui.button(
         "Convert", 
         on_click=lambda: convert_csv_2_json(filename.value),
     )
-    ui.run()
-app()
 
+    ui.run()
+    
+app()
